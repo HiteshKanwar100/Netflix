e635c48bf785fe89a05e805b0d43ea073741191b
Hitesh Kanwar <hitesh.kanwar@brightchamps.com>
2023-10-08 01:05:02 +0530

    fix problem in registering user

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,17 +1,39 @@
-"use client"; // This is a client component
 import axios from "axios";
 import { useCallback, useState } from "react";
-import Input from "@/components/Input";
-import { signIn } from "next-auth/react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
+import Input from "@/components/Input";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   const [variant, setVariant] = useState("login");
-  const [showError, setShowError] = useState(false);
+
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
       currentVariant === "login" ? "register" : "login"
@@ -20,139 +42,106 @@ const Auth = () => {
 
   const login = useCallback(async () => {
     try {
-      signIn("credentials", {
+      await signIn("credentials", {
         email,
         password,
-        callbackUrl: "/profiles",
+        redirect: false,
+        callbackUrl: "/",
       });
+
+      router.push("/profiles");
     } catch (error) {
       console.log(error);
-      setShowError(false);
     }
-  }, [email, password]);
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
-      axios.post("/api/register", {
+      await axios.post("/api/register", {
         email,
         name,
         password,
       });
+
       login();
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
     }
   }, [email, name, password, login]);
 
   return (
-    <>
-      <div className="relative h-full w-full bg-[url('/Images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover overflow-hidden ">
-        <div className="bg-black w-full h-full lg:bg-opacity-50">
-          <nav className="px-12 py-5">
-            <img src="/Images/logo.png" alt="Logo" className="h-12"></img>
-          </nav>
-          <div className="flex justify-center">
-            <div className="bg-black bg-opacity-70 px-12 py-9 self-center  lg:w-2/5 lg:max-w-md rounded-md w-full">
-              <div className="text-white text-4xl mb-8 font-semibold">
-                <h2>{variant === "login" ? "Sign In" : "Create an Account"}</h2>
-              </div>
-              <div className="flex flex-col gap-4">
-                {showError && (
-                  <p className="text-red-700">
-                    Email or Password Wrong.{" "}
-                    <strong className="text-white font-thin">
-                      Please Try Again
-                    </strong>
-                  </p>
-                )}
-                {variant === "register" && (
-                  <Input
-                    id="name"
-                    value={name}
-                    label="Username"
-                    onChange={(ev: any) => {
-                      setName(ev.target.value);
-                    }}
-                  ></Input>
-                )}
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/Images/logo.png" className="h-12" alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
                 <Input
-                  type="email"
-                  id="email"
-                  value={email}
-                  label="Email"
-                  onChange={(ev: any) => {
-                    setEmail(ev.target.value);
-                  }}
-                ></Input>
-                <Input
-                  type="password"
-                  id="password"
-                  value={password}
-                  label="Password"
-                  onChange={(ev: any) => {
-                    setPassword(ev.target.value);
-                  }}
-                ></Input>
-              </div>
-              <button
-                onClick={variant === "login" ? login : register}
-                className="bg-red-600 py-3 rounded-md text-white w-full mt-10 hover:bg-red-700 transition"
-              >
-                {variant === "login" ? "Log In" : "Sign Up"}
-              </button>
-              <div className="flex flex-row items-center justify-center mt-8 gap-4">
-                <div
-                  onClick={() => {
-                    signIn("google", { callbackUrl: "/profiles" });
-                  }}
-                  className="
-                                w-10
-                                h-10
-                                bg-white
-                                rounded-full
-                                flex
-                                items-center
-                                justify-center
-                                cursor-pointer
-                                hover:opacity-80
-                                transition
-                            "
-                >
-                  <FcGoogle size={30} />
-                </div>
-                <div
-                  onClick={() => {
-                    signIn("github", { callbackUrl: "/profiles" });
-                  }}
-                  className="
-                                w-10
-                                h-10
-                                bg-white
-                                rounded-full
-                                flex
-                                items-center
-                                justify-center
-                                cursor-pointer
-                                hover:opacity-80
-                                transition
-                            "
-                >
-                  <FaGithub size={30} />
-                </div>
-              </div>
-              <p onClick={toggleVariant} className="text-neutral-500 mt-5">
-                {variant === "login"
-                  ? "First time using Netflix?"
-                  : "Already have an Account?"}
-                <span className="text-white ml-1 hover:underline cursor-pointer">
-                  {variant === "login" ? "Create an Account" : " Log In"}
-                </span>
-              </p>
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              )}
+              <Input
+                id="email"
+                type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
             </div>
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={32} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={32} />
+              </div>
+            </div>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+              .
+            </p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default Auth;
